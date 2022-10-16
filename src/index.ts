bc0f226ca49d1f6f02adabc499832603ae8bb916
hollowdll <pixerchat@gmail.com>
2022-10-16 15:49:47 +0300

    more array code

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,34 @@ function primitives() {
     
     console.log(typeof myNumber);
 }
+// primitives();
 
 
 /* Arrays */
 
 function arrays() {
+    console.log("Executing function 'arrays'");
+
     let myArray: number[] = [1,2,3];
 
     for (const e of myArray) {
         console.log(e);
     }
 }
+// arrays();
 
 const createTypeScriptArray = () => {
+    console.log("Executing function 'createTypeScriptArray'");
 
+    let myArray: string[] = [];
+
+    for (let i: number = 0; i < 5; i++) {
+        myArray[i] = `text ${i +1}`;
+    }
+
+    myArray.forEach(n => console.log(n, "Length: ", n.length));
 }
+// createTypeScriptArray();
 
 
 /* Tuples */
