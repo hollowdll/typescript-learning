979c8a2eb309565c5c2ff7eeea33a2c8e63330ce
hollowdll <pixerchat@gmail.com>
2022-12-03 17:01:37 +0200

    add new example

--- src/async-functions.ts
+++ src/async-functions.ts
@@ -2,6 +2,7 @@
 // to see different behaviours
 
 let myString: string = "empty";
+let myString2: string = "also empty";
 
 const asyncFunction1 = async (): Promise<void> => {
     let myPromise: Promise<string> = new Promise(function(resolve) {
@@ -15,5 +16,35 @@ const asyncFunction1 = async (): Promise<void> => {
     }
 }
 
+const asyncFunction2 = async (): Promise<void> => {
+    const value: number = 1;
+
+    let myPromise: Promise<string> = new Promise(function(resolve, reject) {
+        if (value !== 1) {
+            resolve("I am asyncFunction2!");
+        } else {
+            reject("Error occurred!");
+        }
+    });
+
+    myPromise.then(
+        function(value: string) { console.log(value); },
+        function(error: any = 0) { console.log(error); }
+    );
+
+    console.log(myPromise);
+
+    try {
+        myString2 = await myPromise;
+        console.log(myString2);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+asyncFunction2();
+
+/*
 asyncFunction1();
 console.log(myString);
+*/
